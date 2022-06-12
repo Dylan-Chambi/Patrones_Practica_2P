f110a9708e397c7947c09bad234f07f154f0e693
Dylan_Chambi <dylanchambifi@gmail.com>
2022-06-11 23:11:22 -0400

    IntelliJ Idea .gitignore configuration

--- src/main/java/ejercicio1/exercise/Test.java
+++ src/main/java/ejercicio1/exercise/Test.java
@@ -3,5 +3,6 @@ package ejercicio1.exercise;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Ejercicio 1");
+        System.out.println("Ejercicio 1");
     }
 }
